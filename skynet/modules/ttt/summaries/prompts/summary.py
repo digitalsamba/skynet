717f84da358fa70111df02bdd7b7d77be33f10ad
Conal Mullan <cmullan@digitalsamba.com>
2024-05-03 19:37:44 +0100

    prompt for ise over ize

--- skynet/modules/ttt/summaries/prompts/summary.py
+++ skynet/modules/ttt/summaries/prompts/summary.py
@@ -1,12 +1,12 @@
 summary_conversation_prompt = """
-  Provide a summary of the following conversation. Do not start with things like 'Sure! Here is ...'. Just summarize it.
+  Provide a summary of the following conversation using British dialect. Do not start with things like 'Sure! Here is ...'. Just summarise it.
 
   Here is the conversation:
 
 """
 
 summary_text_prompt = """
-    Provide a summary of the following text. Do not start with things like 'Sure! Here is ...'. Just summarize it.
+    Provide a summary of the following text using British dialect. Do not start with things like 'Sure! Here is ...'. Just summarise it.
     
     Here is the text:
 
